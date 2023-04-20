a050094bbdce37f74b8b89e7faec0935bdef9a4a
sachindsilva16 <sachindsilvad@gmail.com>
2023-04-20 12:08:56 +0530

    Added Component files

---
+++ src/ReactComponents/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header"
+
+function App(){
+    return(
+        <Header/>
+        <div className="container ">Hello World</div>
+    )
+}
+
+export default App;
